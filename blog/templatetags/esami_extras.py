c779710628d6e2d62d61a0a76ff0d83015fe70a8
abdo <abd.nh25@gmail.com>
2021-06-25 14:09:18 +0300

    Move all custom tags to a single file

--- blog/templatetags/esami_extras.py
+++ blog/templatetags/esami_extras.py
@@ -1,8 +1,25 @@
 from django import template
 from django.utils.safestring import mark_safe
 from django.urls import reverse
+from blog.models import Category
 
 register = template.Library()
+
+@register.simple_tag()
+def get_item(dictionary, key, default):
+    return dictionary.get(key, default)
+
+@register.simple_tag()
+def category_list(label, from_obj):
+    category_rels = from_obj.categories.through.objects.filter(**{f'{from_obj.__class__.__name__.lower()}_id': from_obj.id})
+    if len(category_rels) < 1:
+        return mark_safe('')
+    out = []
+    for category_rel in category_rels:
+        category = Category.objects.get(pk=category_rel.category_id)
+        out.append(f'<a href="{ category.get_absolute_url() }">{ category.name }</a>')
+
+    return mark_safe(label + ' ' + '، '.join(out))
 
 @register.simple_tag
 def update_icon(obj, update_url=None):
--- blog/templatetags/misc_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-# @register.filter
-@register.simple_tag()
-def get_item(dictionary, key, default):
-    return dictionary.get(key, default)
