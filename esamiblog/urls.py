544dc156bbacd38e6a17e42d01be933bed46d37c
abdo <abd.nh25@gmail.com>
2021-07-12 02:19:19 +0300

    Append a slash to forgotten 'post-detail' url

--- esamiblog/urls.py
+++ esamiblog/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post-list'),
     path('posts/feed/', LatestPostsFeed()),
     path('post/create/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     
